09b9e37fb42661caebb0c8d44197bf776184a235
Sage Watson <sagewatson@developersinstitute.co.nz>
2022-01-18 12:01:33 +1300

    Added social links, images, formatting to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,46 @@
-import {Facebook, Instagram, Twitter, Youtube} from "./icons";
+import { Facebook, Instagram, Twitter, Youtube } from "./icons";
+import Image from "next/image";
 
 const Footer = () => (
-	<div className="footer bg-gray-800 p-6 text-white">
-		<div className="container mx-auto">
-			<div className="footer-text flex-none md:flex items-center justify-between">
-				<p>© Codeytek Academy 2020</p>
-				<p className="text-gray">Learn the latest tech skills</p>
-				<span className="text-gray">Follow on social links to support the work</span>
-			</div>
-			<ul className="social-links mt-8 flex align-center">
-				<li><a href="https://www.facebook.com/codeytek" className="fa fa-facebook" target="_blank"><Facebook/></a></li>
-				<li className="ml-2 mt-1"><a href="https://twitter.com/codeytek" target="_blank"><Twitter/></a></li>
-				<li className="ml-2 mt-1"><a href="https://youtube.com/ImranSayedDev" className="fa fa-youtube" target="_blank"><Youtube/></a></li>
-				<li className="ml-2"><a href="https://www.instagram.com/codeytek_academy/" className="fa fa-instagram" target="_blank"><Instagram/></a></li>
-			</ul>
-		</div>
-	</div>
+  <div className="footer bg-black p-6 text-white">
+    <div className="container mx-auto">
+      <div className="footer-text flex-none md:flex items-center justify-between text-xl">
+        <p className="social-text">Follow us on social media</p>
+        <span className="text-center">
+          © Fussey 2022. Website by Sage Watson
+        </span>
+        <span className="text-center stripe-logo">
+          <Image
+            src="https://my-wordpress.fussey.co/wp-content/uploads/2022/01/Stripe_company-Powered-by-Stripe-Logo.wine_-e1641454508312.png"
+            alt="Stripe Logo"
+            width="237"
+            height="72.7"
+          />
+        </span>
+      </div>
+      <ul className="social-links mt-2 flex">
+        <li className="">
+          <a
+            href="https://www.facebook.com/whysofussey"
+            className="fa fa-facebook"
+            target="_blank"
+          >
+            <Facebook />
+          </a>
+        </li>
+
+        <li className="ml-2">
+          <a
+            href="https://www.instagram.com/fussey_/?hl=en"
+            className="fa fa-instagram"
+            target="_blank"
+          >
+            <Instagram />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
 );
 
 export default Footer;
