070f8a167e9c8abdc844fa7007c9ad71a0b1787a
Sage Watson <sagewatson@developersinstitute.co.nz>
2022-01-21 12:47:45 +1300

    Temporarily removed image optimization for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
-const allowedImageWordPressDomain = new URL(
-  process.env.NEXT_PUBLIC_WORDPRESS_URL
-).hostname;
+// const allowedImageWordPressDomain = new URL(
+//   process.env.NEXT_PUBLIC_WORDPRESS_URL
+// ).hostname;
 
 module.exports = {
   trailingSlash: true,
@@ -19,7 +19,6 @@ module.exports = {
   /**
    * We specify which domains are allowed to be optimized.
    * This is needed to ensure that external urls can't be abused.
-   * @see https://nextjs.org/docs/basic-features/image-optimization#domains
    */
   //   images: {
   //     domains: [allowedImageWordPressDomain, "via.placeholder.com"],
