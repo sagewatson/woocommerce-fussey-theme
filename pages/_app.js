56184c003df44e1c892b94bd93fdf5fec88c1c1d
Sage Watson <sagewatson@developersinstitute.co.nz>
2022-02-09 21:03:32 +1300

    Add facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,34 @@
 import "../src/styles/style.scss";
 import "../src/styles/main.scss";
 
-import Router from 'next/router';
-import NProgress from 'nprogress';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import Router from "next/router";
+import NProgress from "nprogress";
 
 NProgress.configure({ showSpinner: false });
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init("3960122867349467"); // facebookPixelId
+        ReactPixel.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
-
+export default MyApp;
